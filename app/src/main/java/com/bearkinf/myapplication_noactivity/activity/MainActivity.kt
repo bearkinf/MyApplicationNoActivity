1947d12f3af05673483763eaf1ea14dfab67c844
bearkinf <36253795+bearkinf@users.noreply.github.com>
2020-09-16 15:03:55 +0900

    Initial commit

---
+++ app/src/main/java/com/bearkinf/myapplication_noactivity/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bearkinf.myapplication_noactivity.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bearkinf.myapplication_noactivity.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/bearkinf/myapplication_noactivity/activity/base/BaseActivity.kt
@@ -0,0 +1,8 @@
+package com.bearkinf.myapplication_noactivity.activity.base
+
+import androidx.appcompat.app.AppCompatActivity
+
+class BaseActivity :AppCompatActivity(){
+
+
+}
